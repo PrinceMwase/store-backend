ff94e25077b21315d8f57e9c68e24679fcd0ac4e
PrinceMwase <princefranklinemwase@gmail.com>
2021-10-17 14:35:12 +0200

    App\Description Relationship definition

--- app/Description.php
+++ app/Description.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Description extends Model
 {
-    //
+
+    protected $fillable = ['description'];
+    //RelationShip Definition
+    
+    public function describable(){
+        return $this->morphTo();
+    }
 }
